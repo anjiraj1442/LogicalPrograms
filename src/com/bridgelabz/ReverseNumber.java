4c44aad03a0b16536be89e2802228b48855a9109
anjirajardula <anjirajardula@gmail.com>
2021-10-02 15:57:50 +0530

    Added: given number changes to reverse number

---
+++ src/com/bridgelabz/ReverseNumber.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * Reverse Number
+ *
+ * @author : anji raj
+ * @version :16.0
+ * @since : 02/10/2021
+ */
+
+public class ReverseNumber {
+
+    //Ask User To Enter a Number
+    //And Write Condition To Print Reverse Number
+
+    public static void main(String[] args) {
+        int num, reminder, reverse = 0;
+        //Welcome message added
+        System.out.println("The program to find Revrese of given Number");
+        //Ask user to enter a number
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        num = sc.nextInt();
+        //While condition for to print the reverse number
+        while (num != 0) {
+
+            reminder = num % 10;
+            reverse = reverse * 10 + reminder;
+            num = num / 10;
+        }
+        System.out.println("The Reverse number is :" + reverse);
+    }
+}
